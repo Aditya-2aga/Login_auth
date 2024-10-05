e68d15414a538aca9140972cee2d85061645ebcf
Aditya-2aga <aditya.2aga@gmail.com>
2024-10-05 10:15:41 +0530

    Changes for file upload

--- models/user.js
+++ models/user.js
@@ -8,6 +8,10 @@ const userSchema = mongoose.Schema({
     email: String,
     age: Number,
     password: String,
+    displaypic: {
+        type: String,
+        default: "default.jpg"
+    },
     posts: [
         {type: mongoose.Schema.Types.ObjectId, ref: "post"}
     ]
